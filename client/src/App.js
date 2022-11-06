135502f6c4e8121e6f0ee7834d6dddd007cf649c
HarpratapsinhNakoom <ui20cs26@iiitsurat.ac.in>
2022-11-06 11:21:01 +0530

    Made navbar and defined colors
    A simple navbar is made and some global colors are defined to be used during the entire coding journey.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import Compiler from './pages/Compiler';
-// import styled from 'styled-components'
 
-// const Container = styled.div``
 function App() {
   return (
-      // <Container>
+      <>
           <Compiler />
-      // </Container>
+      </>
   );
 }
 
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar-container'>
+          <div className="navbar-textContainer">
+              <span id='navbar-maintext'>LearnCodeGroww</span> &nbsp; &nbsp;
+              <span id='navbar-minitext'>Online code compiler</span>
+          </div>
+    </div>
+  )
+}
+
+export default Navbar
